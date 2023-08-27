116e554d714ec0f3f7b842a1a05844dc19e2bf89
Alan Biju <u7315765@anu.edu.au>
2023-08-28 00:09:51 +1000

    Changes to homepage to make it better for audit. Still requires chart to be added and user info is hard coded

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react'
 import AuthContext from '../context/AuthContext'
+import Avatar from 'react-avatar';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     let [documents, setDocuments] = useState([]);
@@ -30,19 +32,47 @@ const Homepage = () => {
     }
 
     return (
-        <div>
-            <div>
-                <p>You are logged to the home page! </p>
+        <body>
+            <div class="column sidemenu">
+                <header>
+                    <h1>Your Details</h1> 
+                </header> 
+                <Avatar name="Alan Biju" size={150} round={true} />
+                <div>
+                <br/>
+                <text><b>Username:</b> alan</text>
+                <br/>
+                <br/>
+                <text><b>First Name:</b> Alan</text>
+                <br/>
+                <br/>
+                <text><b>Last Name:</b> Biju</text>
+                <br/>
+                <br/>
+                <text><b>Email:</b> altalanbiju@gmail.com</text>
+                <br/>
+                </div>
+            </div>
+            <div class="column content">
+                <div class ="contentheader">
+                    <h1>Home Page</h1>
+                </div>
 
-                <p>Here is the list of all the documents:</p> 
+                <p>Here is the list of all the documents you've recently uploaded:</p> 
 
                 <ul>
                     {documents.map((document) => {
-                        return <li key={document.id}>{document.body}</li>
+                        return <li key={document.id}>{document.filename}</li>
                     })}
                 </ul>
+                <Link to="/upload">
+                    <button class="button1">
+                        <p>Upload more files</p>
+                    </button>
+                </Link>
             </div>
-        </div>
+            <footer id="footer"></footer>
+        </body>
     )
 }
 
