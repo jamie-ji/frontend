88046c4e019a6fa7eeeeb7490e7b3090df2c849e
jijianming <u7550825@anu.edu.au>
2023-09-25 16:40:47 +1000

    commit1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,35 @@
 import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
+import '../css/Header.css'
 
 const Header = () => {
     let {user, logoutUser} = useContext(AuthContext)
     return (
-        <div>
-            <Link to="/"><button>Home</button></Link>
-            <span> | </span>
-            {user ? (<button onClick={logoutUser}>Logout</button>) : (<Link to="/login"><button>Login</button></Link>)}
-            <span> | </span>
-            {user ? (<Link to="/upload"><button>Upload</button></Link>) : ''}
-            
-            {user && <p>Hello {user.username}!</p>}
-            
+        <div className="header-container">
+            <Link to="/" className="nav-link">
+                <button className="nav-button">Home</button>
+            </Link>
+            <span className="nav-separator"> | </span>
+            {user ? (
+                <button className="nav-button" onClick={logoutUser}>
+                    Logout
+                </button>
+            ) : (
+                <Link to="/login" className="nav-link">
+                    <button className="nav-button">Login</button>
+                </Link>
+            )}
+            <span className="nav-separator"> | </span>
+            {user ? (
+                <Link to="/upload" className="nav-link">
+                    <button className="nav-button">Upload</button>
+                </Link>
+            ) : (
+                ''
+            )}
+
+            {user && <p className="user-greeting">Hello {user.username}!</p>}
         </div>
     )
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react'
 import AuthContext from '../context/AuthContext'
+import '../css/Login.css'
 
 const LoginPage = () => {
     let {loginUser} = useContext(AuthContext)
     return (
-        <div>
-            <h1>Login Page</h1>
-            <form onSubmit={loginUser}>
-                <input type="text" name="username" placeholder="Username" />
-                <input type="password" name="password" placeholder="Password" /> 
-                <button type="submit">Login</button> 
+        <div className="login-container">
+            <h1 className="login-header">Login Page</h1>
+            <form className="login-form" onSubmit={loginUser}>
+                <input className="login-input" type="text" name="username" placeholder="Username" />
+                <input className="login-input" type="password" name="password" placeholder="Password" />
+                <button className="login-button" type="submit">Login</button>
             </form>
         </div>
     )
